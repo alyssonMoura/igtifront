5eca2d2e42b650e2c8204a990106b1ce98a0f3bb
Alysson Moura <alyssonmoura367@gmail.com>
2026-02-22 16:33:45 -0300

    Criação do .env para ocultar credenciais

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3003/';
+
 export default axios.create({
-  baseURL: 'http://localhost:3003/',
+  baseURL,
   headers: {
     'Content-type': 'application/json',
   },
